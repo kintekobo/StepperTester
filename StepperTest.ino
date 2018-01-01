672aa796ffa9e1a324f23b36bf005f5a344bfd22
Chris <chris.mower@gmail.com>
2018-01-01 14:55:20 +0000

    First release of program Version 0.1

--- StepperTest.ino
+++ StepperTest.ino
@@ -1,7 +1,7 @@
 /* 
  *      Simple program for testing the stepper motor and stepper driver
  *      
- *      Start with the STEP_PULSE_WIDTH set to 100 and the INTER_PULSE_DELAY AT 1000
+ *      Start with the STEP_PULSE_WIDTH set to 20 and the INTER_PULSE_DELAY AT 500
  *      Set REVOLUTIONS to 1 then play with the STEPS_PER_REVOLUTION value
  *      until the stepper motor can be made to do one complete revolution per burst
  *      Once you have it rotating correctly gradually reduce the STEP_PULSE_WIDTH until it
@@ -14,12 +14,15 @@
  
 // Arduino pin usage
 
-// Output pins
+// Output pins on the Arduino UNO
 #define STEP_PIN        3   // Output to the PULSE input on the stepper driver unit
 #define DIR_PIN         4   // Output to the DIR input on the stepper driver unit
 #define ENABLE_PIN      5   // Output to the ENABLE input on the stepper driver unit
 
+// **** IMPORTANT ****
 // These are the values for the switch settings on my stepper driver
+// Change them to match the values for your stepper driver
+//
 // #define STEPS_PER_REVOLUTION 384 // OFF ON OFF
 // #define STEPS_PER_REVOLUTION 768 // OFF OFF ON
 #define STEPS_PER_REVOLUTION 1536   // OFF OFF OFF
@@ -27,12 +30,13 @@
 // Start-up values to give a reasonable functionality
 #define REVOLUTIONS 1   // How many complete revolutions of the motor
 #define NUMBER_OF_STEPS ( STEPS_PER_REVOLUTION * REVOLUTIONS )  // How many step pulses to output per loop
-#define STEP_PULSE_WIDTH 5          // Width of the step pulse in microseconds
-#define INTER_PULSE_DELAY 70       // Microseconds between pulses
+#define STEP_PULSE_WIDTH 20          // Width of the step pulse in microseconds
+#define INTER_PULSE_DELAY 500       // Microseconds between pulses
+#define INTER_PULSE_MINIMUM 10      // Minimum time between pulses
 #define INTER_LOOP_DELAY 1000       // How many milliseconds between rotation bursts           
-#define OVERHEAD_DELAY 4            // Step delay function overhead
+#define OVERHEAD_DELAY 4            // Step delay function overhead in microseconds
 
-enum rotationDirection { CW, CCW };
+enum rotationDirection { CW, CCW } CurrentDir = CW; // Holds the current direction of rotation
 volatile unsigned long int StepPulseWidth = STEP_PULSE_WIDTH - OVERHEAD_DELAY; // Set sensible initial values
 volatile unsigned long int InterPulseDelay = INTER_PULSE_DELAY;
 volatile unsigned long int NumberOfSteps = NUMBER_OF_STEPS;
@@ -40,25 +44,27 @@ volatile unsigned long int NumberOfSteps = NUMBER_OF_STEPS;
 void setup() 
 {
 
-    Serial.begin(115200);
+    Serial.begin( 115200 ); // Initialise the serial port
     
     // Set the Arduino pins as inputs or outputs as required
     pinMode( STEP_PIN, OUTPUT );
     pinMode( DIR_PIN, OUTPUT );
     pinMode( ENABLE_PIN, OUTPUT );
-    Serial.println( "Starting" );
-    Serial.println( "Wxxx to change pulse width" );
-    Serial.println( "Pxxx to change inter-pulse period" );
+    setEnable( false );
+    // Now print the instructions for usage
+    Serial.println( "Usage:" );
+    Serial.println( "W xxx to change pulse width" );
+    Serial.println( "P xxx to change inter-pulse period" );
     Serial.println( "  - where xxx is a time in microseconds" );
-    Serial.println( "Example W100 is 100uSec pulse width and P2000000 is 2 seconds between pulses" );
-    Serial.println( "Nxxx to change number of steps" );
+    Serial.println( "  Example W 100 is 100uSec pulse width and P 2000000 is 2 seconds between pulses" );
+    Serial.println( "N xxx to change number of steps" );
+    Serial.println( "D to change rotation direction" );
     Serial.println( "? to print current values" );
     Serial.println( "ok>" );
 }
 
 void loop() 
-{
-    
+{   
     doStep();
     delay( INTER_LOOP_DELAY );
 }
@@ -81,11 +87,11 @@ void setDirection( rotationDirection Direction )
 {
     if ( Direction == CW )
     {
-        digitalWrite( DIR_PIN, LOW );           
+        digitalWrite( DIR_PIN, HIGH );           
     }
     else
     {
-        digitalWrite( DIR_PIN, HIGH );
+        digitalWrite( DIR_PIN, LOW );
     }
 
 }
@@ -94,10 +100,11 @@ void setDirection( rotationDirection Direction )
 void doStep()
 {
     setEnable( true );
+    setDirection( CurrentDir );
     for ( unsigned long int x = 0; x < NumberOfSteps; ++x )
     {
         digitalWrite( STEP_PIN, HIGH );
-        delayMicroseconds( StepPulseWidth );
+        delayMicroseconds( StepPulseWidth ); // There is a 4uS additional time taken in writing the pins
         digitalWrite( STEP_PIN, LOW ); 
         delayMicroseconds( InterPulseDelay );
     }
@@ -107,59 +114,73 @@ void doStep()
 // This method gets called when serial data is available
 void serialEvent()
 {
-    int i = Serial.peek(); // Examine the first character to see if it is a command
+    char buffer[ 40 ] = { 0 };  // String to hold the formatted data. Keep an eye on this to make sure we don't exceed it
+
+    int iCommand = Serial.peek(); // Examine the first character to see if it is a command
     unsigned long l = Serial.parseInt(); // Get any numeric values if present
+
     if ( Serial.available() > 0 )
-        Serial.readString(); // Flush the input buffer
-        
-    char s[40];  // String to hold the formatted data. Keep an eye on this to make sure we don't exceed it
-    
-    switch ( i ) // If command valid then prosess it
+        Serial.readString(); // Flush the input buffer of anything left by Serial.parseInt() function       
+
+    // Here is where we parse the command and carry out the requested task
+    switch ( iCommand ) // If command valid then process it
     {
         case 0x77: // W & w
-        case 0x57:  if ( l > 4 )
+        case 0x57:  if ( l > OVERHEAD_DELAY ) // Check the size of pulse requested
                     {
-                        StepPulseWidth = ( l - 4 );
-                        sprintf( s, "Pulse width set to %u uSec", l);
+                        StepPulseWidth = ( l - OVERHEAD_DELAY ); // Remove the overhead to get the correct width
+                        sprintf( buffer, "Pulse width set to %u uSec", l);
                     }
                    else
-                       sprintf( s, "ERROR: 5 uSec is the minimum" );
-                    
+                       sprintf( buffer, "ERROR: 5 uSec is the minimum" );                   
             break;
             
         case 0x70: // P & p
-        case 0x50: InterPulseDelay = l;
-                    sprintf( s, "Inter pulse delay set to %u uSec", l );
+        case 0x50: InterPulseDelay = ( l > INTER_PULSE_MINIMUM ) ? l : INTER_PULSE_MINIMUM; // Don't go below INTER_PULSE_MINIMUM
+                    sprintf( buffer, "Inter pulse delay set to %u uSec", InterPulseDelay );
             break;
  
         case 0x6E: // N & n 
-        case 0x4E:  NumberOfSteps = l;  
-                    sprintf( s, "Number of steps set to %u", l );
-            break;  
-        case 0x3F: printValues();
-                    s[0] = 0;
+        case 0x4E:  NumberOfSteps = ( l > 0 ) ? l : 1;  // Always at least one step
+                    sprintf( buffer, "Number of steps set to %u", NumberOfSteps );
+            break;
+              
+        case 0x3F:  // ?
+                    printValues(); // This is the ? command
+                    buffer[ 0 ] = 0; // Clear the buffer to avoid a bug in the compiler
             break; 
-        default:   sprintf( s, "ERROR: Command not recognised - %c", i );
+
+        case 0x44:  // D & d
+        case 0x64:  CurrentDir = ( CurrentDir == CW ) ? CCW:CW; // Change direction of rotation 
+                    sprintf( buffer, "Now rotating %s", (CurrentDir == CW)?"Clockwise":"Counter Clockwise" );      
+            break; 
+
+        default:   sprintf( buffer, "ERROR: Command not recognised - %c", iCommand );
             break;        
-    } 
-    Serial.println( s );
-    
+    }
+    if ( buffer[ 0 ] != 0 )
+        Serial.println( buffer );
+    Serial.println( "ok>" );    
 }
 
 void printValues()
 {
-    char t[ 40 ]; // String to hold the formatted text.
+    char buffer[ 40 ]; // String to hold the formatted text.
     Serial.println( "--------------------------------------------" );
-    sprintf( t, "Pulse width %u uSec", (StepPulseWidth + OVERHEAD_DELAY ));
-    Serial.println( t );
-    sprintf( t, "Inter pulse delay %u uSec",InterPulseDelay );
-    Serial.println( t );
-    sprintf( t, "Number of steps %u", NumberOfSteps );
-    Serial.println( t );
+    sprintf( buffer, "Pulse width %u uSec", (StepPulseWidth + OVERHEAD_DELAY ));
+    Serial.println( buffer );
+    sprintf( buffer, "Inter pulse delay %u uSec",InterPulseDelay );
+    Serial.println( buffer );
+    sprintf( buffer, "Number of steps %u", NumberOfSteps );
+    Serial.println( buffer );
+    sprintf( buffer, "Now rotating %s", (CurrentDir == CW)?"Clockwise":"Counter Clockwise" );
+    Serial.println( buffer );
     Serial.println( "--------------------------------------------" );
+    Serial.println( "ok>" );
 }
 
-void __serialEvent()
+// I use this for testing sometimes. You can remove it if you like
+void _serialEvent() 
 {
     int i = Serial.peek();
     long l = Serial.parseInt();
